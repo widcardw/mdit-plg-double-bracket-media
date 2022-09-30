7fe3a30f2ea5ae81c6edd146af1009675224cdba
widcardw <lui747353367@qq.com>
2022-09-30 21:44:45 +0800

    finish plugin and test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,77 @@
-export const one = 1
-export const two = 2
+import type MarkdownIt from 'markdown-it'
+
+interface DoubleBracketMediaProps {
+  /**
+     * Remove the prefix of the url in double brackets
+     */
+  removePrefix?: string
+  /**
+     * After removing the provided prefix, param `addPrefix` will be added to the front of url
+     */
+  addPrefix?: string
+}
+
+const DoubleBracketMedia: MarkdownIt.PluginWithOptions<DoubleBracketMediaProps> = (md: markdownit, options = {}) => {
+  md.block.ruler.before('paragraph', 'dbm', (state, startLine: number) => {
+    let token
+    const pos = state.bMarks[startLine] + state.tShift[startLine]
+    const max = state.eMarks[startLine]
+
+    const ch = state.src.charCodeAt(pos)
+
+    if (ch !== 0x21 || pos >= max)
+      return false
+
+    const text = state.src.substring(pos, max)
+
+    const regex = new RegExp(`^!\\[\\[${options.removePrefix || ''}(.*?)\\]\\]`)
+
+    const match = text.match(regex)
+
+    if (match && match.length) {
+      const media = match[1]
+
+      let [link, alt] = media.split('|')
+
+      if (!alt || alt.trim() === '')
+        alt = link
+
+      if (options.addPrefix)
+        link = options.addPrefix + link
+
+      if (!link.match(/(.+?)\.([A-Za-z0-9]{1,4})/))
+        return false
+
+      let result = ''
+
+      if (link.endsWith('.mp4') || link.endsWith('.webm'))
+        result = `<video controls="controls" src="/${link}" alt="${alt}" style="width: 100%; height: 100%;"></video>`
+
+      else if (link.endsWith('.mp3'))
+        result = `<audio controls="controls" src="/${link}" alt="${alt}"></audio>`
+
+      else
+        result = `![${alt}](/${link})`
+
+      // markdown it 的处理操作
+      token = state.push('paragraph_open', 'p', 1)
+      token.markup = '@'
+      token.map = [startLine, state.line]
+
+      token = state.push('inline', '', 0)
+      token.content = result
+      token.map = [startLine, state.line]
+      token.children = []
+
+      token = state.push('paragraph_close', 'p', -1)
+      token.markup = '@'
+
+      state.line = startLine + 1
+      return true
+    }
+
+    return false
+  })
+}
+
+export default DoubleBracketMedia
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,42 @@
+import MarkdownIt from 'markdown-it'
 import { describe, expect, it } from 'vitest'
+import DoubleBracketMedia from '../src/index'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('media', () => {
+  it('should parse pictures', () => {
+    const md = new MarkdownIt()
+    md.use(DoubleBracketMedia)
+    expect(md.render('![[path/to/image.jpg]]')).toMatchInlineSnapshot(`
+      "<p><img src=\\"/path/to/image.jpg\\" alt=\\"path/to/image.jpg\\"></p>
+      "
+    `)
+  })
+
+  it('should remove given prefix and add provided certain prefix', () => {
+    const md = new MarkdownIt()
+    md.use(DoubleBracketMedia, {
+      removePrefix: 'public/',
+      addPrefix: 'src/',
+    })
+    expect(md.render('![[public/avatar.png|This is avatar]]')).toMatchInlineSnapshot(`
+      "<p><img src=\\"/src/avatar.png\\" alt=\\"This is avatar\\"></p>
+      "
+    `)
+  })
+
+  it('should parse videos and audios', () => {
+    const md = new MarkdownIt()
+    md.use(DoubleBracketMedia, {
+      removePrefix: 'public/',
+    })
+    expect(md.render('![[public/src/example.mp4]]')).toMatchInlineSnapshot(`
+      "<p>&lt;video controls=&quot;controls&quot; src=&quot;/src/example.mp4&quot; alt=&quot;src/example.mp4&quot; style=&quot;width: 100%; height: 100%;&quot;&gt;&lt;/video&gt;</p>
+      "
+    `)
+
+    expect(md.render('![[public/src/audio.mp3]]')).toMatchInlineSnapshot(`
+      "<p>&lt;audio controls=&quot;controls&quot; src=&quot;/src/audio.mp3&quot; alt=&quot;src/audio.mp3&quot;&gt;&lt;/audio&gt;</p>
+      "
+    `)
   })
 })
